4ffa0d4464ad109c647db463c014cdef8ee86778
Kosaro <okosarkosarewicz@gmail.com>
2017-11-06 10:28:32 -0500

    Fixed typo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAutonomousJustJewel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAutonomousJustJewel.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
  * Created by okosa on 9/29/2017.
  */
 //@Disabled
-@Autonomous(name = "Red Autonomous Just Jewel ")
+@Autonomous(name = "Red Autonomous Just Jewel")
 public class RedAutonomousJustJewel extends RelicRecoveryAutonomous {
     @Override
     Hardware.ColorDetected getDesiredColor() {
